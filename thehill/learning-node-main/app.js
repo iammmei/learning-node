9eeb2d83c1b24d92b372955dd7c1bd677a3ea3db
Safa <safae.khess@gmail.com>
2023-07-27 11:29:16 +0200

    ...

--- thehill/learning-node-main/app.js
+++ thehill/learning-node-main/app.js
@@ -12,6 +12,10 @@ app.get('/alien',function(req,res){
     res.send("Welcome back alien ")
 })
 
+app.get('/alien/:id',function(req,res){
+    const id = req.params.id
+    res.send("Here's the door beach "+ id)
+})
 app.listen(9000,function(req,res){
     console.log("Hi Ken")
 })
